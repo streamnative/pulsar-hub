0c904d0ead67b47d1b7882780ea70037a6c73afa
Li Li <urfree@apache.org>
2023-05-29 11:43:28 +0800

    update
    Signed-off-by: Li Li <urfree@apache.org>

--- fix-tab.js
+++ fix-tab.js
@@ -31,27 +31,21 @@ function fix(mdpath) {
   console.log(mdpath + " fixed");
 }
 
-// module.exports = (dir) => {
-//   travel(dir, fix);
-// };
-
-// Example: node scripts/add-canonical.js versioned_docs/version-2.11.x/adaptors-kafka.md
-// if (typeof require !== "undefined" && require.main === module) {
-const [dir] = process.argv.slice(2);
+// Example: node fix-tab.js connectors/aws-lambda-sink/2.10.0/aws-lambda-sink.md
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const [dir] = process.argv.slice(2);
 travel(path.join(__dirname, dir), fix);
-// }
 
 // Usage:
 // Example 1: Update a specify md file:
-// node scripts/add-canonical.js versioned_docs/version-2.11.x/adaptors-kafka.md
+// node fix-tab.js connectors/aws-lambda-sink/2.10.0/aws-lambda-sink.md
 
 // Example 2: Update files in a specify directory:
-// node scripts/add-canonical.js versioned_docs/version-2.11.x
+// node fix-tab.js connectors/aws-lambda-sink
 
-// Example 3: Update all old version docs:
-// node scripts/add-canonical.js versioned_docs
+// Example 3: Update all connectors docs:
+// node fix-tab.js connectors
 
-// Example 4: Update all current version docs:
-// node scripts/add-canonical.js docs
+// Example 4: Update all docs:
+// node fix-tab.js .
